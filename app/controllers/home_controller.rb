6a380289fade47b3b6c13a79cebe06875e492f4e
Phil Pirozhkov <pirjsuka@gmail.com>
2015-09-15 14:09:44 +0300

    news, docs, quest

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     render :index, locals: {
+      docs: [],
       news: NewsEntry.actual(3),
+      questionnaires: [],
       links: Link.all
     }
   end
